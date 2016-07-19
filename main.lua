2f5a0bb272f5717f89cf125f2942f1e031c30a9d
Ubuntu <ubuntu@ip-172-31-30-242.us-west-2.compute.internal>
2016-07-19 02:41:00 +0000

    server fixes

--- main.lua
+++ main.lua
@@ -20,34 +20,63 @@ end
 local networkEntities = {}
 
 Client = class()
-function Client:init(ip)
+function Client:init(ip, port)
     self.ip = ip
+    self.port = port
 end
 function Client:send(data)
-    udp:sendto(data, self.ip, port)
+    udp:sendto(data, self.ip, self.port)
+    print("sending "..data.." to "..self.ip..":"..self.port)
 end
 local clients = {}
+
+function print_r (t)
+    local print_r_cache={}
+    local function sub_print_r(t,indent)
+        if (print_r_cache[tostring(t)]) then
+            print(indent.."*"..tostring(t))
+        else
+            print_r_cache[tostring(t)]=true
+            if (type(t)=="table") then
+                for pos,val in pairs(t) do
+                    if (type(val)=="table") then
+                        print(indent.."["..pos.."] => "..tostring(t).." {")
+                        sub_print_r(val,indent..string.rep(" ",string.len(pos)+8))
+                        print(indent..string.rep(" ",string.len(pos)+6).."}")
+                    else
+                        print(indent.."["..pos.."] => "..tostring(val))
+                    end
+                end
+            else
+                print(indent..tostring(t))
+            end
+        end
+    end
+    sub_print_r(t,"  ")
+end
 
 print "Beginning server loop."
 while running do
     bindata, msg_or_ip, port_or_nil = udp:receivefrom()
     if bindata then
-        local ip = msg_or_ip
+        local ip, port = msg_or_ip, port_or_nil
         local client = clients[ip]
-        if not client then
-            client = new Client(ip)
+        if not client or port ~= client.port then
+            client = Client(ip, port)
             clients[ip] = client
         end
         local cmd, data = binser.deserializeN(bindata, 2)
         if cmd == "requestId" then
+            print ("requestId from "..msg_or_ip)
             local response = {}
             client.id = nextId
             nextId = nextId + 1
             response.id = client.id
             client.entity = NetworkEntity({x = 5088, y = 4000})
-            networkEntities[id] = client.entity
+            networkEntities[client.id] = client.entity
             response.state = client.entity.state
-            client:send(binser.serialize(response))
+            client:send(binser.serialize("assignId", response))
+            print_r(response)
         elseif cmd == "updateState" then
             networkEntities[data.id].state = data.state
         else
@@ -59,5 +88,5 @@ while running do
  
     socket.sleep(0.01)
 end
- 
+
 print "Thank you."
